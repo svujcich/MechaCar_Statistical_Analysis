960e0cc8aa745c8d6afd015431e4ad3e9ee82aa2
Sarah <sarah.vujcich.school@gmail.com>
2022-09-27 11:24:32 -0700

    adding code for part 2 of challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,6 @@
 library(dplyr)
+
+#1. Linear regression to predict MPG
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
@@ -6,3 +8,9 @@ lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, MechaCar_mpg))
+
+#2. Consistency of suspension coils across production lots
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #read in data
+
+total_summary  <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') #create summary table
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') # group by lot #
