5da301441af0d3fee9aded0b74fc615aad086e81
Sarah <sarah.vujcich.school@gmail.com>
2022-09-27 08:15:57 -0700

    adding linear regression code

---
+++ MechaCarChallenge.R
@@ -0,0 +1,8 @@
+library(dplyr)
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data = MechaCar_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+             ground_clearance + AWD, MechaCar_mpg))
